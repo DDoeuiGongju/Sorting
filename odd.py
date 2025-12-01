0cbb6a900160dbacac3e07b834dad8bd83402832
SOEUI KIM <58338918+DDoeuiGongju@users.noreply.github.com>
2025-12-01 18:14:18 +0900

    Update odd.py

--- odd.py
+++ odd.py
@@ -3,6 +3,7 @@ import random
 import time
 import matplotlib.pyplot as plt
 import platform
+import koreanize_matplotlib
 
 # ------------------- 한글 폰트 설정 -------------------
 system_name = platform.system()
